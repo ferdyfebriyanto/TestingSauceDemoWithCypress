89d991460932a4d432fc75924e466dd165e30a51
= <=>
2021-10-31 17:41:05 +0700

    14-MuhammadNavisAbdillah

---
+++ cypress/integration/6-ResetAppState-MuhammadNavisA/ResetAppState-2.js
@@ -0,0 +1,18 @@
+describe('Login', function (){
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+    })
+    
+    it('Login', () => {
+    
+        cy.get('#user-name').type('locked_out_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').should('be.visible').click()
+        cy.get('#add-to-cart-sauce-labs-backpack').should('be.visible').click()
+        cy.get('#react-burger-menu-btn').should('be.visible').click()
+        cy.get('#reset_sidebar_link').should('be.visible').click()
+        
+
+    })
+    
+})
