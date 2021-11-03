f2d144ad7eb262daa091037eb98d710e310db0c5
= <=>
2021-11-03 14:51:58 +0700

    14-MuhammadNavisAbdillah

--- cypress/integration/6-ResetAppState-MuhammadNavisA/ResetAppState-2.js
+++ cypress/integration/6-ResetAppState-MuhammadNavisA/ResetAppState-2.js
@@ -4,11 +4,15 @@ describe('Login', function (){
     })
     
     it('Login', () => {
-    
+        // Testing Login
         cy.get('#user-name').type('locked_out_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').should('be.visible').click()
+       
+        // Testing Menambahkan Barang
         cy.get('#add-to-cart-sauce-labs-backpack').should('be.visible').click()
+       
+        // Testing Reset App State
         cy.get('#react-burger-menu-btn').should('be.visible').click()
         cy.get('#reset_sidebar_link').should('be.visible').click()
         
--- cypress/integration/6-ResetAppState-MuhammadNavisA/ResetAppState.js
+++ cypress/integration/6-ResetAppState-MuhammadNavisA/ResetAppState.js
@@ -4,11 +4,16 @@ describe('Login', function (){
     })
     
     it('Login', () => {
-    
+        
+        // Testing Login
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').should('be.visible').click()
+        
+        // Testing Menambahkan Barang
         cy.get('#add-to-cart-sauce-labs-backpack').should('be.visible').click()
+
+        // Testing Reset App State
         cy.get('#react-burger-menu-btn').should('be.visible').click()
         cy.get('#reset_sidebar_link').should('be.visible').click()
         
